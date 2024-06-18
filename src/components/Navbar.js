3f33ec4d4727dd1ede38a8626eff0ef98d8197e3
ywon0095 <ywon0095@student.monash.edu>
2024-06-18 14:57:04 +0800

    updated site name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { HashLink as Link } from 'react-router-hash-link';
 function Navbar() {
   return (
     <nav className="container h-[6.25rem] flex justify-between items-center px-5 md:px-12">
-      <h2>Site Name</h2>
+      <h2>Yuan Yi's Page</h2>
       <ul className="flex">
         <li className="pl-5 md:pl-10"><Link smooth to="#home">Home</Link></li>
         <li className="pl-5 md:pl-10"><Link smooth to="#about">About</Link></li>
